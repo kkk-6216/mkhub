581fd1986264fbab462a216ae82a74b50f0b8ad7
Nursultan <tukhtanazarovnursultan@gmail.com>
2025-03-05 23:10:29 +0600

    Новая страница добавлена

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -68,6 +68,16 @@
             </i>
             <router-link v-if="!isCollapsed" to="/settings">Настройки</router-link>
           </li>
+          <li :class="[ 'flex','items-center','p-2','rounded-lg','pb-4','text-gray-500','hover:text-gray-950','transition','duration-200',
+            { 'text-gray-950': $route.path === '/AddFaculty' }
+          ]">
+            <i class="mdi mdi-book-open-variant mr-2" :class="{ 'mr-0': isCollapsed }">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+              </svg>
+            </i>
+            <router-link v-if="!isCollapsed" to="/AddFaculty">Добавить факультет</router-link>
+          </li>
         </ul>
       </nav>
       <!-- Sign In -->
@@ -92,6 +102,7 @@
           <li class="flex items-center p-2 rounded-lg text-gray-500 hover:text-gray-950  transition duration-200" v-if="isLoggedIn">
             <a href="#" @click.prevent="logout" class="w-full text-center py-2 rounded-md bg-main text-white hover:bg-gray-500">Выйти</a>
           </li>
+
         </ul>
       </div>
     </aside>
