aeb7d828323162984c854a80b05ec60ab2edbe90
Miregia-Aigerim <aigesha031018@gmail.com>
2025-03-06 13:24:08 +0600

    Добавила локальные иконки и настроен Register.vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,37 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./styles/index.css";
-import { createPinia } from "pinia";
-import { useAuthStore } from "@/store/auth.js";
-import {jwtDecode} from "jwt-decode";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Импорт роутера
+import './styles/index.css';
 
-const pinia = createPinia();
 const app = createApp(App);
 
-app.use(pinia);
+// Установка Vue Router
 app.use(router);
 
-const authStore = useAuthStore();
-
-(async () => {
-
-    if (authStore.accessToken && authStore.refreshToken) {
-        try {
-            const decoded = jwtDecode(authStore.accessToken);
-            const now = Math.floor(Date.now() / 1000);
-
-            if (decoded.exp < now) {
-                await authStore.refreshAuthToken();
-            }
-        } catch (error) {
-            console.warn("Ошибка обновления токена:", error.message);
-            await authStore.logout();
-        }
-    } else {
-        console.warn("Не найден accessToken или refreshToken, очищаем localStorage.");
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('refreshToken');
-    }
-    app.mount("#app");
-})();
+app.mount('#app');
