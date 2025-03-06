8c0c3f7bb14abd5fe467a76f12e3971d05fac0e9
Miregia-Aigerim <aigesha031018@gmail.com>
2025-03-06 13:24:40 +0600

    Добавила локальные иконки

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL } from 'node:url' // Добавлен импорт
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url"; // Добавлен импорт
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)) // Добавлена конфигурация alias
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)), // Добавлена конфигурация alias
+    },
+  },
+});
