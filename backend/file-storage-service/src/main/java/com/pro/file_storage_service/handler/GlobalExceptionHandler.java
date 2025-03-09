6edd32efe1ae6034aaba91d9a7118016a4f4e9fe
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-03-09 17:03:34 +0600

    добавлен GlobalExceptionHandler.java

---
+++ backend/file-storage-service/src/main/java/com/pro/file_storage_service/handler/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.pro.file_storage_service.handler;
+
+import com.pro.file_storage_service.exception.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ex.getMessage());
+    }
+}
