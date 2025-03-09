e77c4ec6584da6228a10fed87fd4260173d82d1b
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-03-09 17:05:53 +0600

    изменен FileStorageServiceApplication.java

--- backend/file-storage-service/src/main/java/com/pro/file_storage_service/FileStorageServiceApplication.java
+++ backend/file-storage-service/src/main/java/com/pro/file_storage_service/FileStorageServiceApplication.java
@@ -3,9 +3,11 @@ package com.pro.file_storage_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class FileStorageServiceApplication {
 
 	public static void main(String[] args) {
