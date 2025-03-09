a3845abac7e093003002c1857fd4f1670b861efc
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-03-09 17:02:25 +0600

    добавлен AuthValidationController.java

---
+++ backend/auth-service/src/main/java/com/pro/auth_service/controller/AuthValidationController.java
@@ -0,0 +1,38 @@
+package com.pro.auth_service.controller;
+
+import com.pro.auth_service.dto.UserInfoDto;
+import com.pro.auth_service.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthValidationController {
+
+    private final JwtUtil jwtUtil;
+
+    @GetMapping("/validate")
+    public ResponseEntity<UserInfoDto> validateToken(@RequestHeader("Authorization") String token) {
+
+        String accessToken = token.replace("Bearer ", "");
+
+        if (!jwtUtil.validateAccessToken(accessToken)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        UserInfoDto userInfo = new UserInfoDto(
+                jwtUtil.getIdFromAccessToken(accessToken),
+                jwtUtil.getUsernameFromAccessToken(accessToken),
+                jwtUtil.getRoleFromAccessToken(accessToken)
+        );
+
+        return ResponseEntity.ok(userInfo);
+    }
+}
+
