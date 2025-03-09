37259fb876164dcb56a1ead31c3fc3c5a93db172
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-03-09 17:02:14 +0600

    добавлен AuthResponse.java

--- backend/auth-service/src/main/java/com/pro/auth_service/dto/AuthResponse.java
+++ backend/auth-service/src/main/java/com/pro/auth_service/dto/AuthResponse.java
@@ -1,9 +1,10 @@
 package com.pro.auth_service.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
 
