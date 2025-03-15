85487f3e652662a7624915ce44996b0919d9a22d
Miregia-Aigerim <aigesha031018@gmail.com>
2025-03-15 09:14:19 +0600

    Создала AdminSidebar.vue

---
+++ frontend/src/layouts/AdminLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex h-screen bg-gray-100">
+    <!-- Боковая панель -->
+    <AdminSidebar class="w-32" />
+
+    <!-- Основной контент -->
+    <div class="flex-1 w-full overflow-x-hidden overflow-y-auto p-0">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import AdminSidebar from '../components/AdminSidebar.vue';
+
+export default {
+  name: 'AdminLayout',
+  components: {
+    AdminSidebar
+  },
+  // Нет setup(), так как нет логики, требующей Composition API
+};
+</script>
