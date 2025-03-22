4abe16f96148ed3024e83a9efff92103163d619c
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-03-22 22:07:36 +0600

    Mapper: DepartmentMapper добавлен

---
+++ backend/content-service/src/main/java/com/pro/content_service/mapper/DepartmentMapper.java
@@ -0,0 +1,22 @@
+package com.pro.content_service.mapper;
+
+import com.pro.content_service.dto.department.DepartmentDto;
+import com.pro.content_service.model.entity.Department;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DepartmentMapper {
+
+    public DepartmentDto departmentToDepartmentDto(Department department) {
+        return DepartmentDto.builder()
+                .id(department.getId())
+                .name(department.getName())
+                .description(department.getDescription())
+                .facultyDto(DepartmentDto.FacultyDto.builder()
+                        .id(department.getFaculty().getId())
+                        .name(department.getFaculty().getName())
+                        .build())
+                .build();
+    }
+
+}
