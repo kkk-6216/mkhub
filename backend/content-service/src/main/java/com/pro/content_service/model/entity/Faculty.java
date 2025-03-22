074accfaccc3a0b0d2c03f6ba0d3c859faf97ffd
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-03-22 21:57:38 +0600

    создан сущность Faculty

---
+++ backend/content-service/src/main/java/com/pro/content_service/model/entity/Faculty.java
@@ -0,0 +1,38 @@
+package com.pro.content_service.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "faculties")
+public class Faculty {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "faculty", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Department> departments = new ArrayList<>();
+    
+}
