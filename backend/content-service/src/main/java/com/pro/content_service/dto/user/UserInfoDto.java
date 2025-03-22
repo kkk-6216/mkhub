1e3f622dd3006a442086442d7d367bb714823ddd
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-03-22 22:04:39 +0600

    Dto: UserInfoDto создан

---
+++ backend/content-service/src/main/java/com/pro/content_service/dto/user/UserInfoDto.java
@@ -0,0 +1,19 @@
+package com.pro.content_service.dto.user;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserInfoDto {
+
+    private Long id;
+    private String username;
+    private String role;
+    private Date tokenExpirationAt;
+
+}
