105ea4591a522410e48b95cb2f4709af5f4899c0
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-03-22 22:07:56 +0600

    Mapper: FacultyMapper добавлен

---
+++ backend/content-service/src/main/java/com/pro/content_service/mapper/FacultyMapper.java
@@ -0,0 +1,18 @@
+package com.pro.content_service.mapper;
+
+import com.pro.content_service.dto.faculty.FacultyDto;
+import com.pro.content_service.model.entity.Faculty;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FacultyMapper {
+
+    public FacultyDto facultyToFacultyDto(Faculty faculty) {
+        return FacultyDto.builder()
+                .id(faculty.getId())
+                .name(faculty.getName())
+                .description(faculty.getDescription())
+                .build();
+    }
+
+}
