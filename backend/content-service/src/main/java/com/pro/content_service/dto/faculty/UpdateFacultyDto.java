0467b23ad40960ad9b56314a8eb50c1966df9b13
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-03-22 22:04:18 +0600

    Dto: UpdateFacultyDto создан

---
+++ backend/content-service/src/main/java/com/pro/content_service/dto/faculty/UpdateFacultyDto.java
@@ -0,0 +1,10 @@
+package com.pro.content_service.dto.faculty;
+
+import lombok.Data;
+
+@Data
+public class UpdateFacultyDto {
+    private Long id;
+    private String name;
+    private String description;
+}
