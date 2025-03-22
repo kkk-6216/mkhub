628c52428dd72d21e77d378dcb1924064f78aa3e
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-03-22 22:16:20 +0600

    Repository: FacultyRepository добавлен

---
+++ backend/content-service/src/main/java/com/pro/content_service/repository/FacultyRepository.java
@@ -0,0 +1,9 @@
+package com.pro.content_service.repository;
+
+import com.pro.content_service.model.entity.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+}
