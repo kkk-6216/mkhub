d9b2a6cb40cb49d3e53c7eb747ea62c35843dbbe
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-04 10:23:47 +0600

    class: DataCenterInfo created

---
+++ backend/api-gateway/src/main/java/com/pro/api_gateway/dto/DataCenterInfo.java
@@ -0,0 +1,32 @@
+package com.pro.api_gateway.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DataCenterInfo {
+
+    @JsonProperty("@class")
+    private String className;
+
+    private String name;
+
+    public DataCenterInfo(String className, String name) {
+        this.className = className;
+        this.name = name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
