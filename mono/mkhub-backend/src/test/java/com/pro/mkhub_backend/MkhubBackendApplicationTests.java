ee2c5c0d5913da47157148b1200939d93ec310d7
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-04 19:47:25 +0600

    Версия: Монолит - Проект: mkhub

---
+++ mono/mkhub-backend/src/test/java/com/pro/mkhub_backend/MkhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.mkhub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
