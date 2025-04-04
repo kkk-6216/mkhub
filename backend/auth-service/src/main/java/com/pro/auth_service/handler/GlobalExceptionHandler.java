d034c7be816d8ebba8b20afe8aaf66deb541b38d
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-04 10:25:31 +0600

    class: GlobalExceptionHandler created

---
+++ backend/auth-service/src/main/java/com/pro/auth_service/handler/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.pro.auth_service.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return ResponseEntity.badRequest().body(error);
+    }
+
+}
