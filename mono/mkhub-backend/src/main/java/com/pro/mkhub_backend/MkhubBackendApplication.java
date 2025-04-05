d45b021bf6c1c8e47827b178f191ac63ea8319e6
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-05 15:51:51 +0600

    yml парсер подключен к MkhubBackendApplication

--- mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/MkhubBackendApplication.java
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/MkhubBackendApplication.java
@@ -1,9 +1,12 @@
 package com.pro.mkhub_backend;
 
+import com.pro.mkhub_backend.security.model.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CorsProperties.class)
 public class MkhubBackendApplication {
 
 	public static void main(String[] args) {
