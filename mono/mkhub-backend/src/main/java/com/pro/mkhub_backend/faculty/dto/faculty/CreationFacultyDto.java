9e43cd4468cd36ca591c0a804d2246bd6a998224
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-05 22:15:48 +0600

    создан dto класс: CreationFacultyDto

---
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/faculty/dto/faculty/CreationFacultyDto.java
@@ -0,0 +1,11 @@
+package com.pro.mkhub_backend.faculty.dto.faculty;
+
+import lombok.Data;
+
+@Data
+public class CreationFacultyDto {
+
+    private String name;
+    private String description;
+
+}
