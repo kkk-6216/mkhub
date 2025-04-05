eb64a0b2fd92bc4bc3cf0bfdd340c8f75fd98807
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-05 18:31:44 +0600

    создан класc: GoogleDriveConfig

---
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/file_storage/config/GoogleDriveConfig.java
@@ -0,0 +1,46 @@
+package com.pro.mkhub_backend.file_storage.config;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class GoogleDriveConfig {
+
+    @Value("${google.drive.credentials}")
+    private String serviceAccountKeyPath;
+
+    @Bean
+    public Drive googleDriveBean() throws IOException, GeneralSecurityException {
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+        try (InputStream credentialsStream = getClass().getClassLoader().getResourceAsStream(serviceAccountKeyPath)) {
+            if (credentialsStream == null) {
+                throw new IOException("Google Drive service account key file not found!");
+            }
+
+            GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream)
+                    .createScoped(List.of(DriveScopes.DRIVE));
+
+            return new Drive.Builder(httpTransport, jsonFactory, new HttpCredentialsAdapter(credentials))
+                    .setApplicationName("file-storage-service")
+                    .build();
+        }
+    }
+}
