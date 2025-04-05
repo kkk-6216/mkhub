30bcc12e8fc63bb59ab8048ed9639a664725ac4b
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-05 13:46:12 +0600

    создан новая сущность: User

---
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/auth/model/entity/User.java
@@ -0,0 +1,62 @@
+package com.pro.mkhub_backend.auth.model.entity;
+
+import com.pro.mkhub_backend.auth.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(columnDefinition = "boolean default true")
+    private boolean enabled;
+
+    @Column(columnDefinition = "boolean default true")
+    private boolean accountNonExpired;
+
+    @Column(columnDefinition = "boolean default true")
+    private boolean accountNonLocked;
+
+    @Column(columnDefinition = "boolean default true")
+    private boolean credentialsNonExpired;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.enabled = true;
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+    }
+
+}
