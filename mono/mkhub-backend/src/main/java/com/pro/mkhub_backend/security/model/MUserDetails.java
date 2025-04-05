5395fce9b4ca9508f1bc4606f396b4c3a5d020b9
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-05 13:58:15 +0600

    создан MUserDetails который реализует UserDetails

---
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/security/model/MUserDetails.java
@@ -0,0 +1,63 @@
+package com.pro.mkhub_backend.security.model;
+
+import com.pro.mkhub_backend.auth.model.enums.Role;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MUserDetails implements UserDetails {
+
+    private Long id;
+    private String username;
+    private Role role;
+    private String password;
+
+    private boolean enabled;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(role);
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+}
