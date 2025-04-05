ae01b470c1da2f3e76afb44f1801b98dc3f0a22a
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-05 22:20:15 +0600

    GlobalExceptionHandler: обработчик для ResourceNotFoundException

--- mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/security/handler/GlobalExceptionHandler.java
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/security/handler/GlobalExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.pro.mkhub_backend.security.handler;
 
+import com.pro.mkhub_backend.security.exception.ResourceNotFoundException;
 import com.pro.mkhub_backend.security.exception.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,4 +19,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(ex.getMessage());
     }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        Map<String, Object> errorResponse = new LinkedHashMap<>();
+        errorResponse.put("timestamp", LocalDateTime.now());
+        errorResponse.put("status", HttpStatus.NOT_FOUND.value());
+        errorResponse.put("error", "Resource Not Found");
+        errorResponse.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
 }
