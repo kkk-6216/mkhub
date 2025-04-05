2d117eade1e53226a55e609cd3aef609b68cfacb
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-05 22:17:16 +0600

    создан dto класс: UpdateFacultyDto

--- mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/security/exception/UnauthorizedException.java
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/security/exception/UnauthorizedException.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends RuntimeException {
+
     public UnauthorizedException(String message) {
         super(message);
     }
+
 }
