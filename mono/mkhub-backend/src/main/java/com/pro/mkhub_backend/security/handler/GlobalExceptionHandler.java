5e3f06b6927c7ccf8805a5873fd3e257e18c9ed8
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-05 20:29:58 +0600

    создан handler: GlobalExceptionHandler

---
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/security/handler/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.pro.mkhub_backend.security.handler;
+
+import com.pro.mkhub_backend.security.exception.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ex.getMessage());
+    }
+}
