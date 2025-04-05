026c331a284ebd77ced46e284530fdc578841ae8
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-05 16:20:03 +0600

    создан dto класс: UserAbout

---
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/user/dto/UserAbout.java
@@ -0,0 +1,14 @@
+package com.pro.mkhub_backend.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserAbout {
+
+    private String about;
+
+}
