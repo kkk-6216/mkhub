024404c5619dfa8502ebe6970d37430c93573fe3
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-05 16:04:37 +0600

    создан новый сущность: UserInfo

---
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/user/model/entity/UserInfo.java
@@ -0,0 +1,44 @@
+package com.pro.mkhub_backend.user.model.entity;
+
+import com.pro.mkhub_backend.auth.model.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_details")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String imageUrl;
+    @Column(columnDefinition = "TEXT")
+    private String about;
+    private String phoneNumber;
+    private String firstName;
+    private String lastName;
+    private String instagramLink;
+    private String githubLink;
+
+    @OneToOne(mappedBy = "userInfo")
+    private User user;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
