3f3166296ced621ae2de733cdfa0566f55dd5d40
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-07 18:21:11 +0600

    создан dto класс: UserUsername

---
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/user/dto/UserUsername.java
@@ -0,0 +1,14 @@
+package com.pro.mkhub_backend.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUsername {
+
+    private String username;
+
+}
