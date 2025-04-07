0647a2d39f7b51bf5310b60dd8626b770f0af8fc
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-07 18:22:59 +0600

    UserDetailsDto: расширение dto класса

--- mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/user/dto/UserDetailsDto.java
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/user/dto/UserDetailsDto.java
@@ -1,5 +1,6 @@
 package com.pro.mkhub_backend.user.dto;
 
+import com.pro.mkhub_backend.auth.model.entity.User;
 import com.pro.mkhub_backend.auth.model.enums.Role;
 import com.pro.mkhub_backend.file_storage.model.entity.UserFileMetadata;
 import com.pro.mkhub_backend.file_storage.util.GoogleDriveLinkBuilder;
@@ -13,29 +14,34 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDetailsDto {
+    public class UserDetailsDto {
 
-    private Long id;
-    private String about;
-    private String firstName;
-    private String lastName;
-    private String username;
-    private Role role;
-    private String email;
-    private String phone;
-    private String instagram;
-    private String github;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private String imageUrl;
+        private Long id;
+        private String about;
+        private String firstName;
+        private String lastName;
+        private String username;
+        private Role role;
+        private String email;
+        private String phone;
+        private String instagram;
+        private String github;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+        private String imageUrl;
 
-    public UserDetailsDto(UserInfo userInfo, Role role, GoogleDriveLinkBuilder linkBuilder) {
-        this.id = userInfo.getId();
+        private boolean enabled;
+        private boolean accountNonExpired;
+        private boolean accountNonLocked;
+        private boolean credentialsNonExpired;
+
+    public UserDetailsDto(UserInfo userInfo, User user, GoogleDriveLinkBuilder linkBuilder) {
+        this.id = user.getId();
         this.about = userInfo.getAbout();
         this.firstName = userInfo.getFirstName();
         this.lastName = userInfo.getLastName();
-        this.username = userInfo.getUsername();
-        this.role = role;
+        this.username = user.getUsername();
+        this.role = user.getRole();
         this.phone = userInfo.getPhoneNumber();
         this.email = userInfo.getEmail();
         this.instagram = userInfo.getInstagramLink();
@@ -47,7 +53,12 @@ public class UserDetailsDto {
         if (avatar != null && avatar.getGoogleDriveFileId() != null && !avatar.getGoogleDriveFileId().isEmpty()) {
             this.imageUrl = linkBuilder.buildThumbnailUrl(avatar.getGoogleDriveFileId());
         }
-    }
 
+        this.enabled = user.isEnabled();
+        this.accountNonExpired = user.isAccountNonExpired();
+        this.accountNonLocked = user.isAccountNonLocked();
+        this.credentialsNonExpired = user.isCredentialsNonExpired();
+
+    }
 
 }
