53d88ce4af30b776af696ee3229796c0dc331137
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-07 18:25:50 +0600

    UserService: добавлены новые методы: createUser и update... методы

--- mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/auth/service/UserService.java
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/auth/service/UserService.java
@@ -5,6 +5,9 @@ import com.pro.mkhub_backend.auth.dto.RegistrationRequest;
 import com.pro.mkhub_backend.auth.model.entity.User;
 import com.pro.mkhub_backend.auth.model.enums.Role;
 import com.pro.mkhub_backend.auth.repository.UserRepository;
+import com.pro.mkhub_backend.user.dto.SecureStatus;
+import com.pro.mkhub_backend.user.dto.UserCreationDto;
+import com.pro.mkhub_backend.user.dto.UserItem;
 import com.pro.mkhub_backend.user.model.entity.UserInfo;
 import com.pro.mkhub_backend.user.repository.UserInfoRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -39,7 +43,6 @@ public class UserService {
         User savedUser = userRepository.save(user);
 
         UserInfo userInfo = new UserInfo();
-        userInfo.setUsername(savedUser.getUsername());
         userInfo.setEmail(registrationRequest.getEmail());
 
         savedUser.setUserInfo(userInfo);
@@ -50,6 +53,16 @@ public class UserService {
         return savedUser;
     }
 
+    @Transactional
+    public User createUser(UserCreationDto userCreationDto) {
+        User user = createUser(new RegistrationRequest(
+                userCreationDto.getUsername(),
+                userCreationDto.getEmail(),
+                userCreationDto.getPassword()
+        ));
+        user.setRole(userCreationDto.getRole());
+        return user;
+    }
 
     @Transactional
     public void changePassword(PasswordChangeDto request) {
@@ -74,6 +87,43 @@ public class UserService {
         return userRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("User: " + id + " not found")
         );
+    }
+
+    public List<UserItem> findAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(UserItem::new)
+                .toList();
+    }
+
+    @Transactional
+    public void deleteUserById(Long id) {
+        User user = findById(id);
+        userRepository.delete(user);
+    }
+
+    @Transactional
+    public void updateUsername(Long id, String username) {
+        User user = findById(id);
+        user.setUsername(username);
+        User updated = userRepository.save(user);
+    }
+
+    @Transactional
+    public void updateUserRole(Long id, Role role) {
+        User user = findById(id);
+        user.setRole(role);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void updateSecureStatus(Long id, SecureStatus secureStatus) {
+        User user = findById(id);
+        user.setEnabled(secureStatus.isEnabled());
+        user.setAccountNonExpired(secureStatus.isAccountNonExpired());
+        user.setAccountNonLocked(secureStatus.isAccountNonLocked());
+        user.setCredentialsNonExpired(secureStatus.isCredentialsNonExpired());
+        userRepository.save(user);
     }
 
 }
