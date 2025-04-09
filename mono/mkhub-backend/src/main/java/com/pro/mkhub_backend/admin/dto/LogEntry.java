80561e6c52c427082964686f3e87ac35b612a960
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-09 12:26:23 +0600

    LogEntry: добавлен новые поля

--- mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/admin/dto/LogEntry.java
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/admin/dto/LogEntry.java
@@ -4,15 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LogEntry {
-    private String timestamp;
+
+    private LocalDateTime timestamp;
     private String user;
     private String role;
     private String method;
     private String url;
-    private String duration;
-}
+    private String status;
+    private long duration;
 
+    private String requestBody;
+    private String responseBody;
+
+    private String authorization;
+    private String contentType;
+    private String userAgent;
+    private String ipAddress;
+}
