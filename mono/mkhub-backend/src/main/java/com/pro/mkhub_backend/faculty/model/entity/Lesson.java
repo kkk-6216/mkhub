f88a9a05a012c9e758502085533b3e91ae5e2945
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-04-18 18:04:41 +0600

    создан новый сущность: Lesson

---
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/faculty/model/entity/Lesson.java
@@ -0,0 +1,34 @@
+package com.pro.mkhub_backend.faculty.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "lessons")
+public class Lesson {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    
+}
