261aa1cf3bde9c25420178e44d20885704937f74
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-05-14 17:58:23 +0600

    Добавлен метод findByIdWithBlocks в LessonRepository

--- mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/faculty/repository/LessonRepository.java
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/faculty/repository/LessonRepository.java
@@ -2,8 +2,16 @@ package com.pro.mkhub_backend.faculty.repository;
 
 import com.pro.mkhub_backend.faculty.model.entity.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
+
+    @Query("SELECT l FROM Lesson l JOIN FETCH l.blocks WHERE l.id = :id")
+    Optional<Lesson> findByIdWithBlocks(@Param("id") Long id);
+
 }
