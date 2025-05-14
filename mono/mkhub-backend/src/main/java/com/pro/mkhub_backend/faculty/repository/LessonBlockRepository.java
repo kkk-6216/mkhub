36adb0a717d7b4d63187f76c8d018a560c52b25e
kkk-6216 <ekadyrkulov6216@gmail.com>
2025-05-14 17:55:31 +0600

    "Добавлен репозиторий LessonBlockRepository"

---
+++ mono/mkhub-backend/src/main/java/com/pro/mkhub_backend/faculty/repository/LessonBlockRepository.java
@@ -0,0 +1,9 @@
+package com.pro.mkhub_backend.faculty.repository;
+
+import com.pro.mkhub_backend.faculty.model.entity.LessonBlock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LessonBlockRepository extends JpaRepository<LessonBlock, Long> {
+}
